2eda3166b9f1beead63bf90e98f920789ce105de
Shahanafarvin <shahana50997@gmail.com>
2024-07-25 11:00:36 +0530

    uodated requirements.txt and setup.py version 5

--- setup.py
+++ setup.py
@@ -1,21 +1,35 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='user_agent_filter',  # Package name
-    version='0.1.0',           # Version number
-    description='A package to test and filter user agents for specific websites.',
-    author='Your Name',
-    author_email='your.email@example.com',
-    url='https://github.com/yourusername/user_agent_filter',  # GitHub or project URL
+    name='UserAgentFilter',  # Name of your package
+    version='1.0.0',  # Initial release version
+    description='A package for testing user agents on specific websites',  # Short description
+    long_description=open('README.md').read(),  # Long description read from the README file
+    long_description_content_type='text/markdown',  # Type of the long description content
+    author='Ambily Biju & Shahana Farvin',  # Author's name
+    author_email='ambilybiju2408@gmail.com,shahana50997@gmail.com',  # Author's email
+    url='https://github.com/ambilynanjilath/UserAgentFilter.git',  # URL of the project (e.g., GitHub)
     packages=find_packages(),  # Automatically find packages in the directory
     install_requires=[
-        'requests>=2.0.0',     # Specify the dependencies
+        'requests>=2.25.0',
+        'urllib3>=1.26.0',
     ],
+    python_requires='>=3.7',  # Minimum Python version requirement
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',  # Development status
+        'Intended Audience :: Developers',  # Intended audience
+        'License :: OSI Approved :: MIT License',  # License
+        'Programming Language :: Python :: 3',  # Supported Python versions
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    python_requires='>=3.6',   # Specify the Python version requirement
+    keywords='user agent testing, web scraping, requests',  # Keywords for your package
+    project_urls={
+        'Documentation': 'https://github.com/ambilynanjilath/UserAgentFilter/blob/main/README.md',
+        'Source': 'https://github.com/ambilynanjilath/UserAgentFilter',
+        'Tracker': 'https://github.com/ambilynanjilath/UserAgentFilter/issues',
+    },
 )
